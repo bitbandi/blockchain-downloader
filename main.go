4e2135a5c023f7a07f40efcad84537b08ec01dbf
elbandi <bandispam@gmail.com>
2022-06-11 21:50:09 +0200

    Rewrite error logging

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"bytes"
-	"encoding/hex"
 	"encoding/binary"
+	"encoding/hex"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"os"
@@ -14,6 +15,22 @@ import (
 	"github.com/bitbandi/btcd/wire"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 )
+
+func FatalErr(err error, str string) {
+	if err != nil {
+		log.Fatalf("%s: %s", str, err.Error())
+	}
+}
+
+func CheckErr(err error, str string) {
+	if err != nil {
+		log.Printf("%s: %s", str, err.Error())
+	}
+}
+
+func DeferClose(f io.Closer, str string) {
+	CheckErr(f.Close(), str)
+}
 
 func ReverseString(bytes []byte) string {
 	for left, right := 0, len(bytes)-1; left < right; left, right = left+1, right-1 {
@@ -46,75 +63,53 @@ func main() {
 	protocolVersion := uint32(*protocolVersionPtr) // wire.ProtocolVersion
 
 	tcpAddr, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("%s:%d", *nodeHostnamePtr, *nodePortPtr))
-	if err != nil {
-		log.Fatalln("Resolve address failed:", err.Error())
-	}
+	FatalErr(err, "Resolve address failed")
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
-	if err != nil {
-		log.Fatalln("Connect to node failed:", err.Error())
-	}
-	defer conn.Close()
+	FatalErr(err, "Connect to node failed")
+	defer DeferClose(conn, "Node close failed")
 
 	tcpAddrMe := &net.TCPAddr{IP: net.ParseIP("0.0.0.0"), Port: 0}
 	me := wire.NewNetAddress(tcpAddrMe, wire.SFNodeNetwork)
 	you := wire.NewNetAddress(tcpAddr, wire.SFNodeNetwork)
 	nonce, err := wire.RandomUint64()
-	if err != nil {
-		log.Fatalln("Random failed:", err.Error())
-	}
+	FatalErr(err, "Random failed")
 	msgVer := wire.NewMsgVersion(me, you, nonce, 0)
 	msgVer.AddService(wire.SFNodeNetwork)
 	msgVer.ProtocolVersion = int32(protocolVersion)
 	_, err = wire.WriteMessageWithEncodingN(conn, msgVer, protocolVersion, bitcoinNet, wire.BaseEncoding)
-	if err != nil {
-		log.Fatalln("Write to node failed:", err.Error())
-	}
+	FatalErr(err, "Write to node failed")
 	lastRequestedHash := chainhash.Hash{}
 	requestCount := 0
 	f, err := os.Create(*outFilePtr)
-	if err != nil {
-		log.Fatalln("Create file failed:", err.Error())
-	}
-	defer f.Close()
+	FatalErr(err, "Create file failed")
+	defer DeferClose(f, "Close file failed")
 
 	for {
 		_, rmsg, buf, err := wire.ReadMessageBase(conn, protocolVersion, bitcoinNet)
-		if err != nil {
-			log.Fatalln("Read message failed:", err.Error())
-		}
+		FatalErr(err, "Read message failed")
 		_, ok := rmsg.(*wire.MsgBlock)
 		if !ok { // parse if no MsgBlock type
 			pr := bytes.NewBuffer(buf)
 			err = rmsg.BtcDecode(pr, protocolVersion, wire.BaseEncoding)
-			if err != nil {
-				log.Fatalln("Decode failed:", err.Error())
-			}
+			FatalErr(err, "Decode failed")
 		}
 
 		switch msg := rmsg.(type) {
 		case *wire.MsgVersion:
 			_, err = wire.WriteMessageWithEncodingN(conn, wire.NewMsgVerAck(), protocolVersion, bitcoinNet, wire.BaseEncoding)
-			if err != nil {
-				log.Fatalln("Write to node failed:", err.Error())
-			}
+			FatalErr(err, "Write to node failed")
 		case *wire.MsgPing:
 			_, err = wire.WriteMessageWithEncodingN(conn, wire.NewMsgPong(msg.Nonce), protocolVersion, bitcoinNet, wire.BaseEncoding)
-			if err != nil {
-				log.Fatalln("Write to node failed:", err.Error())
-			}
+			FatalErr(err,"Write to node failed")
 		case *wire.MsgVerAck:
 			getBlocksMsg := wire.NewMsgGetBlocks(&chainhash.Hash{})
 			if len(*startHashPtr) > 0 {
 				startHash, err := chainhash.NewHashFromStr(*startHashPtr)
-				if err != nil {
-					log.Fatalln("Invalid start hash:", err.Error())
-				}
+				FatalErr(err, "Invalid start hash")
 				_ = getBlocksMsg.AddBlockLocatorHash(startHash)
 			}
 			_, err = wire.WriteMessageWithEncodingN(conn, getBlocksMsg, protocolVersion, bitcoinNet, wire.BaseEncoding)
-			if err != nil {
-				log.Fatalln("Write to node failed:", err.Error())
-			}
+			FatalErr(err, "Write to node failed")
 		case *wire.MsgInv:
 			if len(msg.InvList) > 1 {
 				msgGetData := wire.NewMsgGetDataSizeHint(uint(len(msg.InvList)))
@@ -133,9 +128,7 @@ func main() {
 					requestCount++
 				}
 				_, err = wire.WriteMessageWithEncodingN(conn, msgGetData, protocolVersion, bitcoinNet, wire.BaseEncoding)
-				if err != nil {
-					log.Fatalln("Write to node failed:", err.Error())
-				}
+				FatalErr(err, "Write to node failed")
 			}
 		case *wire.MsgBlock:
 			f.Write(messageStartBytes)
@@ -157,9 +150,7 @@ func main() {
 				msgGetBlocks := wire.NewMsgGetBlocks(&chainhash.Hash{})
 				_ = msgGetBlocks.AddBlockLocatorHash(&lastRequestedHash)
 				_, err = wire.WriteMessageWithEncodingN(conn, msgGetBlocks, protocolVersion, bitcoinNet, wire.BaseEncoding)
-				if err != nil {
-					log.Fatalln("Write to node failed:", err.Error())
-				}
+				FatalErr(err, "Write to node failed")
 			}
 		default:
 			println(msg.Command())
